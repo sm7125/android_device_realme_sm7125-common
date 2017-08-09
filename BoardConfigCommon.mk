26d61180f167c5e595505b775fee5ef822073a8b
David Ng <dave@codeaurora.org>
2017-08-08 17:15:24 -0700

    sm7125-common: Add common config.fs AID/fs configuration file
    Common config.fs/$TARGET_FS_CONFIG_GEN AID/fs configuration
    file for all chipset inheriting from base.mk.
    
    Since legacy device-specific android_filesystem_config.h and
    config.fs do not co-exist, temporarily include config.fs only
    if legacy file is absent.  Once all legacy, device-specific
    files have been deleted, config.fs can be made unconditional.
    
    Change-Id: Ie100d0cd9db808b82d99be7139f0709390c7f3a2
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,6 +40,9 @@ ifeq ($(HOST_OS),linux)
 endif
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
