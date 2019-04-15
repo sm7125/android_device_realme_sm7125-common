32ca25b1e0774298cbc32416f1714c66aab4ed91
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    sm7125-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- common.mk
+++ common.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit proprietary targets
 $(call inherit-product-if-exists, vendor/realme/sm7125-common/sm7125-common-vendor.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
