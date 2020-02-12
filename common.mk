2d754543220d7db93e834823bcb247eed37a238c
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    sm7125-common: overlay: Enable freeform windows
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- common.mk
+++ common.mk
@@ -136,6 +136,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     libsensorndkbridge
