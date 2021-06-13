272784163e91ae9b6b9400d1f539306cb3a071a6
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2021-06-13 19:03:07 +0000

    sm7125-common: oppo -> oplus
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- interfaces/vendor/update-makefiles.sh
+++ interfaces/vendor/update-makefiles.sh
@@ -3,4 +3,4 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.oppo:device/realme/sm7125-common/interfaces/vendor"
+  "vendor.oplus:device/realme/sm7125-common/interfaces/vendor"
