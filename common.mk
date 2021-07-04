0c22b042a97684435ef82bd31b40601b916bc7f2
SagarMakhar <sagarmakhar@gmail.com>
2021-07-04 11:19:17 +0000

    sm7125-common: Build Renderscript hal
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- common.mk
+++ common.mk
@@ -258,6 +258,10 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
