e403ec1281d06ad2d9bbf306e41c25c014ad0520
SagarMakhar <sagarmakhar@gmail.com>
2021-07-04 11:17:16 +0000

    sm7125-common: Advertise ipsec-tunnels support
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- common.mk
+++ common.mk
@@ -230,6 +230,10 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor
 
+# Network
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
